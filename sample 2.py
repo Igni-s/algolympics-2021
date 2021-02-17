7bb2f2adefee94cb230511040627a92518916fc4
Igni-s <69242077+Igni-s@users.noreply.github.com>
2021-02-17 18:02:18 +0800

    Update sample 2.py

--- sample 2.py
+++ sample 2.py
@@ -1,5 +1,5 @@
 """
-Sample problem link: https://drive.google.com/file/d/1bjpKoI15oVu5rBxdYYnQbpp6-7e44pu9/view?fbclid=IwAR0FdMmbcw8GYw4nLc26fSlCZlCc9TBeOAVw--NIbVCMxJLGus-9W6pPk64
+Sample problem link: http://bit.ly/Algo2021SP2
 """
 
 import re
